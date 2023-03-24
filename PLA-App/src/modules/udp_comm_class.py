326c298070c02f34986fe02c136318ba1f5c1385
gpizzigh-bit <gabrielpizzighini@gmail.com>
2023-03-24 19:09:36 +0000

    Developt the udp communication to run in a thread

--- PLA-App/src/modules/udp_comm_class.py
+++ PLA-App/src/modules/udp_comm_class.py
@@ -1,22 +1,21 @@
 """
 .. module:: udp_comm_class
    :platform: Unix, Windows
-   :synopsis: Estabilish an UDP comunication to retrieve the orders data.
+   :synopsis: Establish an UDP communication to retrieve the orders data.
 
 .. moduleauthor:: Gabriel Pizzighini <up201800998@up.pt>
 
 
 """
 
-
-
 import socket
 import xml.etree.ElementTree as ET
 from rich.traceback import install
+
 install(show_locals=True)
 
 
-class Orders:
+class ProcessOrders:
     """
     Class that retrieves orders from a udp connection server client XML file
 
@@ -28,38 +27,49 @@ class Orders:
     __aux = True
     _orders_list = []
 
-    def __init__(self,ip,port) -> None:
+    def __init__(self, ip, port) -> None:
         """
         Class constructor
 
         Args:
-            ip (str): ip to estabilish the udp connection
-            port (int): port for estabilishing an udp connection
+            ip (str): ip to establish the udp connection
+            port (int): port for establishing an udp connection
         """
         self.ip = ip
         self.port = port
+        self.SIG_UPDATE = False
         pass
 
     def __connect(self):
-        sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+        print("Waiting for connection...")
+        self.__aux = True
+        sock = socket.socket(socket.AF_INET,  # Internet
+                             socket.SOCK_DGRAM)  # UDP
         sock.bind((self.ip, self.port))
 
         while self.__aux:
-            data, _ = sock.recvfrom(1024) # buffer size is 1024 bytes
+            data, _ = sock.recvfrom(1024)  # buffer size is 1024 bytes
             sock.close()
-            self.__aux = False
+            self.__aux = False  # exit loop
         return data
-    
+
     def get(self):
         """
-        Retrives the data from the cennection
+        Retrieves the data from the connection
         
         Returns:  
-            list (dic) -- the orders in a list of dictionaty.
+            list (dic) -- the orders in a list of dictionary.
         """
         data = self.__connect()
+        print("Received:", data)
         client = ET.fromstring(data)
         for orders in client:
             self._orders_list.append(orders.attrib)
         return self._orders_list
+
+    def start(self):
+        """
+        Starts listening for an udp connection to retrieve data and send to the database.
+        """
+        while True:
+            self.get()

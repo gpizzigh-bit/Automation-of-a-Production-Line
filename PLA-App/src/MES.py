5eacaaa20d98891edc340dbc733e46da18069b0f
diogo-tavares <up201706336@fe.up.pt>
2023-04-21 19:53:49 +0100

    MES communicating with PLC

--- PLA-App/src/MES.py
+++ PLA-App/src/MES.py
@@ -1,11 +1,14 @@
-# [TODO] Communicate to the ERP (Client-side)
-# [TODO] Communicate to the MODBUS interface (OPC-UA over IP)
 # [TODO] Communicate to the Database
 # [TODO] Lost Communication retrive "Last state" from the database
 # [TODO] Execute Orders
 
 import multiprocessing
 from modules import tcp_comm
+
+from opcua import Client
+
+url = "opc.tcp://localhost:4840"
+client = Client(url)
 
 
 def process_tcp_comm():
@@ -20,4 +23,21 @@ def process_tcp_comm():
 if __name__ == '__main__':
     tcp_process = multiprocessing.Process(target=process_tcp_comm)
     tcp_process.start()
-    # continue the code...
+
+
+    try:
+        # Connect to server
+        client.connect()
+
+        # Read node
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.Enter_Piece_Up0.cmd_L")
+        print(f'Value of node : {var.get_value()}')
+
+        # Change value
+        var.set_value(False)
+        print(f'New value is : {var.get_value()}')
+
+
+    finally:
+        client.disconnect()
+

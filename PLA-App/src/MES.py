f02435c12a9167e60f19610942bb5bd8b96c8f81
Diogo <up201907774@g.uporto.pt>
2023-05-15 12:10:42 +0100

    codesys percursos p2

--- PLA-App/src/MES.py
+++ PLA-App/src/MES.py
@@ -7,6 +7,7 @@
 
 from opcua import Client
 
+
 url = "opc.tcp://localhost:4840"
 client = Client(url)
 
@@ -20,7 +21,7 @@ def process_tcp_comm():
         print(f"Received: {message}")
 '''
 
-
+'''
 def wh_m1(client):
 
     try:
@@ -212,14 +213,93 @@ def wh_m3(client):
 
     print("finish")
     return 0
-
+'''
 
 if __name__ == '__main__':
     #tcp_process = multiprocessing.Process(target=process_tcp_comm)
     #tcp_process.start()
 
-    wh_m1(client)
+    try:
+        # Connect to server
+        client.connect()
 
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.TimeMeas.Tempo")
+        timein = var.get_value()
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.WH_out.cmd_R")
+        var.set_value(True)
+
+        var2 = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.TimeMeas.test")
+        var2.set_value(2)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.T1.cmd_R")
+        var.set_value(True)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.T1.cmd_L")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.TR_Up_1.cmd_Rotation_plus")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.TR_Up_1.cmd_Rotation_minus")
+        var.set_value(True)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.TR_Up_1.cmd_L")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.TR_Up_1.cmd_R")
+        var.set_value(True)
+
+        while(True):
+            var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.I[3]")
+            if(var.get_value()==True):
+                break
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.TR_Up_1.cmd_Rotation_plus")
+        var.set_value(True)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.TR_Up_1.cmd_Rotation_minus")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.M1.cmd_R")
+        var.set_value(True)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.M1.cmd_L")
+        var.set_value(False)
+
+        while(True):
+            var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.GVL.I[6]")
+            if (var.get_value() == True):
+                var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.M1.cmd_R")
+                var.set_value(False)
+                break
+        #Desable
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.WH_out.cmd_R")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.T1.cmd_R")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.T1.cmd_L")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.TR_Up_1.cmd_L")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Geral.TR_Up_1.cmd_R")
+        var.set_value(False)
+
+        var = client.get_node("ns=4;s=|var|CODESYS Control Win V3 x64.Application.TimeMeas.Tempo")
+        timeout = var.get_value()
+
+        diftime = timeout-timein
+
+        print(diftime)
+
+    finally:
+        client.disconnect()
+
+    print("finish")
     """
     try:
         # Connect to server
